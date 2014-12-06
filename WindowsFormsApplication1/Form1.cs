2911fdfb6db6650449a3c557acac4380f57e2f58
amuslija1 <amuslija1@etf.unsa.ba>
2014-12-06 02:10:05 +0100

    Initial setup
    Inital setup of the repository.

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<User> users = null;
+        void init()
+        {
+            users = new List<User>();
+            for (int i = 0; i < 10; i++)
+                users.Add(new User());
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+
+            List<User> korisnici = users.FindAll(x => x.Name == "Musa");
+  
+            listBox1.DataSource = users;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            users.Add(new User());
+            DateTime d = DateTime.Now;
+            if (d.Date == dateTimePicker1.Value.Date)
+                users.Add(new User());
+            string s = listBox1.SelectedItem.ToString();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public User(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public User()
+        {
+            Name = "ime";
+            Surname = "prezime";
+        }
+        public override string ToString()
+        {
+            return Name + " " + Surname;
+        }
+    }
+}
